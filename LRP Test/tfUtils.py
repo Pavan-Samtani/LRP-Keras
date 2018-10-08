7abce728e458bf6fe9c9e506702f0abc2bf9951c
Pavan Samtani <pavan3398@gmail.com>
2018-10-08 17:46:21 -0300

    Trained model to test lrp on

--- LRP Test/tfUtils.py
+++ LRP Test/tfUtils.py
@@ -2,20 +2,20 @@ import numpy as np
 
 #random_minibatches: Array(m, ...), Array(m, ...), int, int -> list(tuple(Array(m, ...), Array(m, ...)))
 #Returns a list of shuffled minibatches of the specified size, and using the specified seed.
-def random_minibatches(X, Y, minibatch_size, seed = 69):
-    assert(X.shape[0] == Y.shape[0])
-    m = X.shape[0]
-    order = list(np.random.permutation(m))
+def random_minibatches(X_, Y_, minibatch_size, seed = 69):
+    assert(X_.shape[1] == Y_.shape[1])
+    m = X_.shape[1]
+    order = np.random.permutation(m)
     num_complete_minibatches = m // minibatch_size
-    X = X[order]
-    Y = Y[order]
+    X_ = X_[:, order]
+    Y_ = Y_[:, order]
     j = 0
     minibatches = []
     for i in range(num_complete_minibatches):
-        minibatches.append((X[j : j+minibatch_size], Y[j : j+minibatch_size]))
+        minibatches.append((X_[:, j : j+minibatch_size], Y_[:, j : j+minibatch_size]))
         j += minibatch_size
     if num_complete_minibatches != m/minibatch_size:
-        minibatches.append((X[j : m-1], Y[j : m-1]))
+        minibatches.append((X_[:, j : m], Y_[:, j : m]))
     return minibatches
     
     
