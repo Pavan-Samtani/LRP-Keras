cf8109d7f48a0aa4d69cdeb21ddea64c54ddf080
pavan-samtani <pavan3398@gmail.com>
2018-10-17 18:18:19 -0300

    Got consistent results in relevance analysis, comparing with know relevance regions

--- LRP Test/tfUtils.py
+++ LRP Test/tfUtils.py
@@ -3,19 +3,19 @@ import numpy as np
 #random_minibatches: Array(m, ...), Array(m, ...), int, int -> list(tuple(Array(m, ...), Array(m, ...)))
 #Returns a list of shuffled minibatches of the specified size, and using the specified seed.
 def random_minibatches(X_, Y_, minibatch_size, seed = 69):
-    assert(X_.shape[1] == Y_.shape[1])
-    m = X_.shape[1]
+    assert(X_.shape[0] == Y_.shape[0])
+    m = X_.shape[0]
     order = np.random.permutation(m)
     num_complete_minibatches = m // minibatch_size
-    X_ = X_[:, order]
-    Y_ = Y_[:, order]
+    X_ = X_[order, :]
+    Y_ = Y_[order, :]
     j = 0
     minibatches = []
     for i in range(num_complete_minibatches):
-        minibatches.append((X_[:, j : j+minibatch_size], Y_[:, j : j+minibatch_size]))
+        minibatches.append((X_[j : j+minibatch_size, :], Y_[j : j+minibatch_size, :]))
         j += minibatch_size
     if num_complete_minibatches != m/minibatch_size:
-        minibatches.append((X_[:, j : m], Y_[:, j : m]))
+        minibatches.append((X_[j : m, :], Y_[j : m, :]))
     return minibatches
     
     
